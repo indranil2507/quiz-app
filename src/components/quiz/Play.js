0fed9b8fbf4b5984557ecf3fbb6388279c789033
indranil2507 <iindranilsaha1@gmail.com>
2021-02-09 15:29:09 +0530

    styling

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -15,23 +15,24 @@ class Play extends Component{
             <Helmet><title>Quiz Page</title></Helmet>
           
             <div className="questions">
+                <h2>Quiz Mode</h2>
             <div className="lifeline-container">
                 <p>
-                    <span className="lifeline-icon"><FaLightbulb/></span>
+                    <span className="lifeline-icon"><FaLightbulb/>{" "}<span className="lifeline">5</span></span>
                 </p>
                 <p>
                     <span className="lifeline-icon"><FaStarHalf/></span>
                 </p>
             </div>
-            <div>
+            <div className="timer-container">
                 <p>
-                    <span>1 of 15</span>
-                    2:15
-                <span className="lifeline-icon"><AiOutlineClockCircle/></span>
+                    <span className="left" style={{float: 'left'}}>1 of 15</span>
+                    <span className="right" style={{float: 'right'}}>2:15<AiOutlineClockCircle/></span>
+                
                 </p>
                
             </div>
-            <h1>Google was Founded in what year?</h1>
+            <h5>Google was Founded in what year?</h5>
             <div className="options-container">
                 <p className="options">1997</p>
                 <p className="options">1997</p>
